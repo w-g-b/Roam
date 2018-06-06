e847e7e81ce41bc1cfe86c5ec4c262e4397ca99a
GreenLight <1251191746@qq.com>
2018-06-06 14:50:19 +0800

    添加马桶交互（声音）以及增加屋顶

---
+++ Assets/Scripts/ClosesToolController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClosesToolController : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void OnClickClosesTool()
+    {
+        if (!audioSource.isPlaying)
+            audioSource.Play();
+    }
+}
