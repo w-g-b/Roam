f4e2df7e45dc093533060d62e27bebdc45516a18
GreenLight <1251191746@qq.com>
2018-06-06 14:00:06 +0800

    添加电视控制

---
+++ Assets/Scripts/TVController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class TVController : MonoBehaviour
+{
+    private VideoPlayer vidoePlayer;
+    private bool isPlay = false;
+
+    private void Awake()
+    {
+        vidoePlayer = GetComponent<VideoPlayer>();
+    }
+
+    public void OnClickTV()
+    {
+        if (isPlay)
+        {
+            vidoePlayer.Pause();
+            isPlay = false;
+        }
+        else
+        {
+            vidoePlayer.Play();
+            isPlay = true;
+        }
+    }
+}
